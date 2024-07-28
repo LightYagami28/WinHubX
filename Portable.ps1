6948774413a445504382081b98ba1e7138830adb
Alessandro Maule <maule2703@ik.me>
2024-07-28 10:59:17 +0200

    Update Portable.ps1

--- Portable.ps1
+++ Portable.ps1
@@ -1,23 +1,40 @@
 $URL = "https://github.com/MrNico98/WinHubX/releases/download/WinHubX-v.2.4.0.2/WinHubX.portable.exe"
 $FILE = "WinHubX.portable.exe"
-Invoke-WebRequest -Uri $URL -OutFile $FILE
 
-# Controlla se il download è riuscito
-if (Test-Path $FILE) {
+try {
+    # Scarica il file
+    Write-Output "Inizio download..."
+    Invoke-WebRequest -Uri $URL -OutFile $FILE -UseBasicP
     Write-Output "Download completato."
+
+    # Verifica se il file è stato scaricato
+    if (-Not (Test-Path $FILE)) {
+        throw "Il download non è riuscito. Il file non è stato trovato."
+    }
 
     # Esegui l'applicazione
     Write-Output "Avviando l'applicazione..."
     $process = Start-Process -FilePath $FILE -PassThru
-    
-    # Attendi fino a quando il processo non è terminato
     $process.WaitForExit()
 
+    # Verifica se il processo è stato terminato con successo
+    if ($process.ExitCode -eq 0) {
+        Write-Output "Applicazione terminata con successo."
+    } else {
+        throw "L'applicazione è terminata con codice di uscita non zero: $($process.ExitCode)"
+    }
+
+    # Rimuovi il file
     Write-Output "Eliminando $FILE..."
-    Remove-Item -Path $FILE
+    Remove-Item -Path $FILE -Force
     Write-Output "Operazione completata."
-} else {
-    Write-Output "Il download non è riuscito."
 }
-
-
+catch {
+    Write-Error "Errore: $_"
+}
+finally {
+    # Assicurati che il file venga eliminato anche in caso di errore
+    if (Test-Path $FILE) {
+        Remove-Item -Path $FILE -Force
+    }
+}
